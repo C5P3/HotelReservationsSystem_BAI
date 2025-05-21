283800f009b613e86a8ebae9311d2ec8767d3ad9
C5P3 <elia04@gmx.ch>
2025-05-21 14:16:29 +0200

    Update Address.py

--- source/model/Address.py
+++ source/model/Address.py
@@ -1,17 +1,17 @@
 class Address:
-    def __init__(self, id: int, street: str, city: str, zip: str):
-        self._id = id
+    def __init__(self, address_id: int, street: str, city: str, zip: str):
+        self._address_id = address_id
         self._street = street
         self._city = city
         self._zip = zip
 
     @property
-    def id(self) -> int:
-        return self._id
+    def address_id(self) -> int:
+        return self._address_id
 
     @id.setter
-    def id(self, id: int) -> None:
-        self._id = id
+    def id(self, address_id: int) -> None:
+        self._id = address_id
 
     @property
     def street(self) -> str:
@@ -39,12 +39,12 @@ class Address:
 
     def __str__(self) -> str:
         return "Address(id={0},street={1},zip={2},city={3})".format(
-            self._id, self._street, self._zip, self._city
+            self._address_id, self._street, self._zip, self._city
         )
 
     def to_dict(self):
         return {
-            "id": self._id,
+            "id": self._address_id,
             "street": self._street,
             "zip": self._zip,
             "city": self._city,
