6616bdccc33f02d6b642ed1d651ec99028fa555d
C5P3 <elia04@gmx.ch>
2025-05-21 11:51:03 +0200

    Update Guest.py

--- source/model/Guest.py
+++ source/model/Guest.py
@@ -1,4 +1,8 @@
 class Guest:
-    def __init__(self):
-        pass
+    def __init__(self, guest_id: int, first_name: str, last_name: str, email: str, address_id: int):
+        self._guest_id = guest_id
+        self._first_name = first_name
+        self._last_name = last_name
+        self._email = email
+        self._address_id = address_id
     
