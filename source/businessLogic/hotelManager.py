460f55d77c20001fea0ce547013203f1b944a4c2
Felipe3052 <felipe.wuethrich@students.fhnw.ch>
2025-05-25 21:19:22 +0200

    added User-Story 1.3 and 1.4

--- source/businessLogic/hotelManager.py
+++ source/businessLogic/hotelManager.py
@@ -13,6 +13,13 @@ class HotelManager:
     def search_hotels_by_city_and_stars(self, city: str, stars: int) -> list[Hotel]:
         return self.__hotel_access.get_hotel_by_city_and_stars(city, stars)
     
+    def search_hotels_by_city_and_max_guests(self, city: str, max_guests: int) -> list[Hotel]:
+        return self.__hotel_access.get_hotel_by_city_and_max_guests(city, max_guests)
+    
+    def search_hotels_by_city_and_dates(self, city: str, check_in_date: str, check_out_date: str) -> list[Hotel]:
+        return self.__hotel_access.get_hotel_by_city_and_dates(city, check_in_date, check_out_date)
+
+    
 
 
 
--- source/dataAccess/hotelAccess.py
+++ source/dataAccess/hotelAccess.py
@@ -52,12 +52,60 @@ class HotelAccess(BaseDataAccess):
 
         return hotels
     
+    def get_hotel_by_city_and_max_guests(self, city: str, max_guests: int) -> list[Hotel]:
+
+        query = """
+        SELECT DISTINCT Hotel.hotel_id, Hotel.name, Hotel.stars, Address.address_id, Address.street, Address.city, Address.zip_code
+        FROM Hotel
+        JOIN Address ON Hotel.address_id = Address.address_id
+        JOIN Room ON Hotel.hotel_id = Room.hotel_id
+        JOIN Room_Type ON Room.type_id = Room_Type.type_id
+        WHERE Address.city = ?
+        AND Room_Type.max_guests >= ?
+        """
+        params = (city, max_guests)
+        results = self.fetchall(query, params)
+
+        hotels = []
+        for row in results:
+            hotel_id, hotel_name, hotel_stars, address_id, address_street, address_city, address_zip = row
+            address = Address(address_id, address_street, address_city, address_zip)
+            hotel = Hotel(hotel_id, hotel_name, hotel_stars, address)
+            hotels.append(hotel)
+
+        return hotels
     
-    
+    def get_hotel_by_city_and_dates(self, city: str, check_in_date: str, check_out_date: str) -> list[Hotel]:
+        query = """
+        SELECT DISTINCT Hotel.hotel_id, Hotel.name, Hotel.stars, Address.address_id, Address.street, Address.city, Address.zip_code
+        FROM Hotel
+        JOIN Address ON Hotel.address_id = Address.address_id
+        WHERE Address.city = ?
+        AND Hotel.hotel_id IN (
+            SELECT hotel_id
+            FROM available_hotel_rooms
+            WHERE city = ?
+            AND room_id NOT IN (
+                SELECT room_id
+                FROM Booking
+                WHERE NOT (
+                    Booking.check_out_date <= ?
+                    OR Booking.check_in_date >= ?
+                )
+            )
+        );
+        """
+        params = (city, city, check_in_date, check_out_date)
+        results = self.fetchall(query, params)
+
+        hotels = []
+        for row in results:
+            hotel_id, hotel_name, hotel_stars, address_id, address_street, address_city, address_zip = row
+            address = Address(address_id, address_street, address_city, address_zip)
+            hotel = Hotel(hotel_id, hotel_name, hotel_stars, address)
+            hotels.append(hotel)
+
+        return hotels
 
 
-        
-
-
-        
 
